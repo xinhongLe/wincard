e13283c0fba2e2e285d8f885a5dee8d05efe5be6
moneyinto <moneyinto@moneyintodeMacBook-Pro.local>
2021-11-02 16:21:57 +0800

    事件编辑及优化

--- src/views/Editor/Toolbar/ElementEvent.vue
+++ src/views/Editor/Toolbar/ElementEvent.vue
@@ -1,156 +1,165 @@
 <template>
     <div class="el-event-box">
-        <div class="tip"><IconClick />该元件点击触发 请选择触发对象</div>
-        <a-form
-            :model="formState"
-            :label-col="{ span: 7 }"
-            :wrapper-col="{ span: 17 }"
+        <div class="tip"><IconClick />该元素点击触发事件</div>
+        <a-modal
+            v-model:visible="addActionVisible"
+            title="事件"
+            width="300px"
+            okText="保存"
+            cancelText="取消"
+            @ok="isEdit ? editAction() : addAction()"
         >
-            <a-form-item label="目标元素:">
-                <a-select
-                    v-model:value="formState.target"
-                    placeholder="请选择目标元素"
-                >
-                    <a-select-option
-                        :value="item.id"
-                        v-for="item in elementList"
-                        :key="item.id"
-                        >{{ item.name }}</a-select-option
+            <a-form
+                :model="formState"
+                :label-col="{ span: 7 }"
+                :wrapper-col="{ span: 17 }"
+            >
+                <a-form-item label="目标元素:">
+                    <a-select
+                        v-model:value="formState.target"
+                        placeholder="请选择目标元素"
                     >
-                </a-select>
-            </a-form-item>
+                        <a-select-option
+                            :value="item.id"
+                            v-for="item in elementList"
+                            :key="item.id"
+                            >{{ item.name }}</a-select-option
+                        >
+                    </a-select>
+                </a-form-item>
 
-            <a-form-item label="触发事件:">
-                <a-select
-                    v-model:value="formState.type"
-                    placeholder="请选择触发事件"
-                >
-                    <a-select-option
-                        :value="item.type"
-                        v-for="item in actions"
-                        :key="item.type"
-                        >{{ item.name }}</a-select-option
+                <a-form-item label="触发事件:">
+                    <a-select
+                        v-model:value="formState.type"
+                        placeholder="请选择触发事件"
                     >
-                </a-select>
-            </a-form-item>
+                        <a-select-option
+                            :value="item.type"
+                            v-for="item in actions"
+                            :key="item.type"
+                            >{{ item.name }}</a-select-option
+                        >
+                    </a-select>
+                </a-form-item>
 
-            <a-form-item label="进入动画:">
-                <a-popover
-                    trigger="click"
-                    v-model:visible="inAnimationPoolVisible"
-                >
-                    <template #content>
-                        <div class="animation-pool">
-                            <div
-                                class="pool-type"
-                                v-for="type in inAnimations"
-                                :key="type.name"
-                            >
-                                <div class="type-title">{{ type.name }}：</div>
-                                <div class="pool-item-wrapper">
-                                    <div
-                                        class="pool-item"
-                                        v-for="item in type.children"
-                                        :key="item.name"
-                                        @mouseenter="
-                                            hoverPreviewAnimation = item.value
-                                        "
-                                        @mouseleave="hoverPreviewAnimation = ''"
-                                        @click="addAnimation(item.value, 'in')"
-                                    >
+                <a-form-item label="进入动画:">
+                    <a-popover
+                        trigger="click"
+                        v-model:visible="inAnimationPoolVisible"
+                    >
+                        <template #content>
+                            <div class="animation-pool">
+                                <div
+                                    class="pool-type"
+                                    v-for="type in inAnimations"
+                                    :key="type.name"
+                                >
+                                    <div class="type-title">{{ type.name }}：</div>
+                                    <div class="pool-item-wrapper">
                                         <div
-                                            class="animation-box"
-                                            :class="[
-                                                'animate__animated',
-                                                'animate__faster',
-                                                hoverPreviewAnimation ===
-                                                    item.value &&
-                                                    `animate__${item.value}`
-                                            ]"
+                                            class="pool-item"
+                                            v-for="item in type.children"
+                                            :key="item.name"
+                                            @mouseenter="
+                                                hoverPreviewAnimation = item.value
+                                            "
+                                            @mouseleave="hoverPreviewAnimation = ''"
+                                            @click="addAnimation(item.value, 'in')"
                                         >
-                                            {{ item.name }}
+                                            <div
+                                                class="animation-box"
+                                                :class="[
+                                                    'animate__animated',
+                                                    'animate__faster',
+                                                    hoverPreviewAnimation ===
+                                                        item.value &&
+                                                        `animate__${item.value}`
+                                                ]"
+                                            >
+                                                {{ item.name }}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </template>
-                    <a-button class="element-animation-btn">
-                        <IconEffects style="margin-right: 5px" />
-                        {{
-                            formState.inAni
-                                ? animationTypes[formState.inAni]
-                                : "点击选择动画"
-                        }}
-                    </a-button>
-                </a-popover>
-            </a-form-item>
+                        </template>
+                        <a-button class="element-animation-btn">
+                            <IconEffects style="margin-right: 5px" />
+                            {{
+                                formState.inAni
+                                    ? animationTypes[formState.inAni]
+                                    : "点击选择动画"
+                            }}
+                        </a-button>
+                    </a-popover>
+                </a-form-item>
 
-            <a-form-item label="退出动画:">
-                <a-popover
-                    trigger="click"
-                    v-model:visible="outAnimationPoolVisible"
-                >
-                    <template #content>
-                        <div class="animation-pool">
-                            <div
-                                class="pool-type"
-                                v-for="type in outAnimations"
-                                :key="type.name"
-                            >
-                                <div class="type-title">{{ type.name }}：</div>
-                                <div class="pool-item-wrapper">
-                                    <div
-                                        class="pool-item"
-                                        v-for="item in type.children"
-                                        :key="item.name"
-                                        @mouseenter="
-                                            hoverPreviewAnimation = item.value
-                                        "
-                                        @mouseleave="hoverPreviewAnimation = ''"
-                                        @click="addAnimation(item.value, 'out')"
-                                    >
+                <a-form-item label="退出动画:">
+                    <a-popover
+                        trigger="click"
+                        v-model:visible="outAnimationPoolVisible"
+                    >
+                        <template #content>
+                            <div class="animation-pool">
+                                <div
+                                    class="pool-type"
+                                    v-for="type in outAnimations"
+                                    :key="type.name"
+                                >
+                                    <div class="type-title">{{ type.name }}：</div>
+                                    <div class="pool-item-wrapper">
                                         <div
-                                            class="animation-box"
-                                            :class="[
-                                                'animate__animated',
-                                                'animate__faster',
-                                                hoverPreviewAnimation ===
-                                                    item.value &&
-                                                    `animate__${item.value}`
-                                            ]"
+                                            class="pool-item"
+                                            v-for="item in type.children"
+                                            :key="item.name"
+                                            @mouseenter="
+                                                hoverPreviewAnimation = item.value
+                                            "
+                                            @mouseleave="hoverPreviewAnimation = ''"
+                                            @click="addAnimation(item.value, 'out')"
                                         >
-                                            {{ item.name }}
+                                            <div
+                                                class="animation-box"
+                                                :class="[
+                                                    'animate__animated',
+                                                    'animate__faster',
+                                                    hoverPreviewAnimation ===
+                                                        item.value &&
+                                                        `animate__${item.value}`
+                                                ]"
+                                            >
+                                                {{ item.name }}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </template>
-                    <a-button class="element-animation-btn">
-                        <IconEffects style="margin-right: 5px" />
-                        {{
-                            formState.outAni
-                                ? animationTypes[formState.outAni]
-                                : "点击选择动画"
-                        }}
-                    </a-button>
-                </a-popover>
-            </a-form-item>
+                        </template>
+                        <a-button class="element-animation-btn">
+                            <IconEffects style="margin-right: 5px" />
+                            {{
+                                formState.outAni
+                                    ? animationTypes[formState.outAni]
+                                    : "点击选择动画"
+                            }}
+                        </a-button>
+                    </a-popover>
+                </a-form-item>
 
-            <a-form-item label="延迟时间:">
-                <a-input-number
-                    :min="0"
-                    :max="5000"
-                    :step="100"
-                    :value="formState.duration"
-                    @change="(value) => updateElementAnimationDuration(value)"
-                    style="width: 100%"
-                />
-            </a-form-item>
-        </a-form>
+                <a-form-item label="延迟时间:">
+                    <a-input-number
+                        :min="0"
+                        :max="5000"
+                        :step="100"
+                        :value="formState.duration"
+                        @change="(value) => updateElementAnimationDuration(value)"
+                        style="width: 100%"
+                    />
+                </a-form-item>
+            </a-form>
+        </a-modal>
 
-        <a-button type="primary" block @click="addAction">新增</a-button>
+        <a-button type="primary" block @click="isEdit = false; addActionVisible = true">新增事件</a-button>
 
         <a-divider />
 
@@ -182,6 +191,16 @@
                 </div>
             </a-tooltip>
             <div class="handler">
+                <a-tooltip
+                    :mouseLeaveDelay="0"
+                    :mouseEnterDelay="0.5"
+                    title="编辑"
+                >
+                    <IconEdit
+                        class="handler-btn"
+                        @click="openEditAction(index)"
+                    />
+                </a-tooltip>
                 <a-tooltip
                     :mouseLeaveDelay="0"
                     :mouseEnterDelay="0.5"
@@ -270,6 +289,9 @@ export default defineComponent({
         const hoverPreviewAnimation = ref("");
         const inAnimationPoolVisible = ref(false);
         const outAnimationPoolVisible = ref(false);
+        const addActionVisible = ref(false);
+        const isEdit = ref(false);
+        const editIndex = ref(0);
 
         const addAnimation = (animation: string, type: string) => {
             if (type === "in") formState.inAni = animation;
@@ -284,12 +306,27 @@ export default defineComponent({
 
         const { addHistorySnapshot } = useHistorySnapshot();
 
+        const openEditAction = (i: number) => {
+            editIndex.value = i;
+
+            const _actions: PPTElementAction[] = handleElement.value.actions || [];
+            const _action: PPTElementAction = _actions[i];
+            formState.target = _action.target;
+            formState.type = _action.type;
+            formState.inAni = _action.inAni || "";
+            formState.outAni = _action.outAni || "";
+            formState.duration = _action.duration || 0;
+
+            isEdit.value = true;
+
+            addActionVisible.value = true;
+        };
+
         // 新增事件
         const addAction = () => {
             if (!formState.target) return message.warning("请选择事件目标元素");
 
-            const _actions: PPTElementAction[] =
-                handleElement.value.actions || [];
+            const _actions: PPTElementAction[] = handleElement.value.actions || [];
             _actions.push(JSON.parse(JSON.stringify(formState)));
 
             store.commit(MutationTypes.UPDATE_ELEMENT, {
@@ -299,13 +336,33 @@ export default defineComponent({
                 }
             });
 
+            addActionVisible.value = false;
+
+            addHistorySnapshot();
+        };
+
+        // 编辑事件
+        const editAction = () => {
+            const _actions: PPTElementAction[] = handleElement.value.actions || [];
+            _actions[editIndex.value] = JSON.parse(JSON.stringify(formState));
+
+            store.commit(MutationTypes.UPDATE_ELEMENT, {
+                id: handleElement.value.id,
+                props: {
+                    actions: _actions
+                }
+            });
+
+            addActionVisible.value = false;
+
+            isEdit.value = false;
+
             addHistorySnapshot();
         };
 
         // 删除事件
         const deleteAction = (i: number) => {
-            const _actions: PPTElementAction[] =
-                handleElement.value.actions || [];
+            const _actions: PPTElementAction[] = handleElement.value.actions || [];
             _actions.splice(i, 1);
 
             store.commit(MutationTypes.UPDATE_ELEMENT, {
@@ -328,10 +385,14 @@ export default defineComponent({
             hoverPreviewAnimation,
             inAnimationPoolVisible,
             outAnimationPoolVisible,
+            addActionVisible,
             animationTypes,
+            isEdit,
             addAction,
+            editAction,
             deleteAction,
             addAnimation,
+            openEditAction,
             updateElementAnimationDuration
         };
     }
