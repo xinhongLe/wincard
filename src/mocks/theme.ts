09709a427c714d101764c4936896eb8191668ae2
李新红 <lixinhong@upplus.net>
2022-01-07 11:48:55 +0800

    修改文本默认颜色

--- src/mocks/theme.ts
+++ src/mocks/theme.ts
@@ -2,7 +2,7 @@ import { SlideTheme } from "@/types/slides";
 
 export const theme: SlideTheme = {
     themeColor: "#5b9bd5",
-    fontColor: "#333",
+    fontColor: "#000",
     fontName: "Microsoft Yahei",
     backgroundColor: "#fff",
     fontSize: "20px"
