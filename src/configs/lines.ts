f0a68f1371ce30ad41c61ea648720f719483b9da
moneyinto <woyaoqianjin@gmail.com>
2022-03-22 10:04:11 +0800

    --task=1001313 --user=钱进 窗卡页 教研端 需求优... https://www.tapd.cn/38241030/s/1108854

--- src/configs/lines.ts
+++ src/configs/lines.ts
@@ -20,8 +20,11 @@ export const LINE_LIST: PresetLine[] = [
             { path: "M 0 0 L 20 20", style: "solid", points: ["", ""] },
             { path: "M 0 0 L 20 20", style: "dashed", points: ["", ""] },
             { path: "M 0 0 L 20 20", style: "solid", points: ["", "arrow"] },
+            { path: "M 0 0 L 20 20", style: "solid", points: ["arrow", "arrow"] },
             { path: "M 0 0 L 20 20", style: "dashed", points: ["", "arrow"] },
-            { path: "M 0 0 L 20 20", style: "solid", points: ["", "dot"] }
+            { path: "M 0 0 L 20 20", style: "dashed", points: ["arrow", "arrow"] },
+            { path: "M 0 0 L 20 20", style: "solid", points: ["", "dot"] },
+            { path: "M 0 0 L 20 20", style: "solid", points: ["dot", "dot"] }
         ]
     },
     {
@@ -34,9 +37,21 @@ export const LINE_LIST: PresetLine[] = [
                 isBroken: true
             },
             {
+                path: "M 0 0 L 0 20 L 20 20",
+                style: "solid",
+                points: ["arrow", "arrow"],
+                isBroken: true
+            },
+            {
                 path: "M 0 0 Q 0 20 20 20",
                 style: "solid",
                 points: ["", "arrow"],
+                isCurve: true
+            },
+            {
+                path: "M 0 0 Q 0 20 20 20",
+                style: "solid",
+                points: ["arrow", "arrow"],
                 isCurve: true
             }
         ]
