8a2d4af79c7faace0ab0e5dedefaaea90cb460b4
moneyinto <woyaoqianjin@gmail.com>
2022-06-22 15:56:47 +0800

    自定义动画为空上一步执行问题 --story=1001372 --user=钱进 WEB窗卡页优化（父需求无法建立的任务，在这里建） https://www.tapd.cn/38241030/s/1190291

--- src/hooks/useActionAnimation.ts
+++ src/hooks/useActionAnimation.ts
@@ -27,7 +27,6 @@ export default (slide: Ref<Slide>) => {
         const animation = noAnimation ? "" : animationType === "show" ? action.inAni : action.outAni;
         if (action.type === "none") return;
         const elRef: HTMLElement | null = document.querySelector(`#screen-element-${action.target} [class^=base-element-]`) || document.querySelector(`#screen-element-${action.target} [class^=editable-element-]`);
-
         setTimeout(() => {
             if (elRef) {
                 // 如果当前效果和点击触发效果一样将不执行下面的逻辑
@@ -41,11 +40,13 @@ export default (slide: Ref<Slide>) => {
                     return setDisplay(false, element.id);
                 }
 
+                // 处理来回动画切换 动画不执行问题 先清空动画 延时100ms后重新赋值
+                // 放在判断里面当返回的时候不触发
+                (<any>elRef).style.animation = null;
+                (<any>elRef).style.offsetPath = null;
+
                 // 判断是否为自定义动画
                 if (CUSTOM_ANIMATIONS.indexOf(animation || "") > -1) {
-                    // 处理来回动画切换 动画不执行问题 先清空动画 延时100ms后重新赋值
-                    (<any>elRef).style.animation = null;
-                    (<any>elRef).style.offsetPath = null;
                     setTimeout(() => {
                         // 如果是执行显示动画，需要先将display设置为true
                         if (animationType === "show") {
@@ -56,7 +57,7 @@ export default (slide: Ref<Slide>) => {
                         const path = noAnimation ? "" : animationType === "show" ? action.inPath : action.outPath;
                         (<any>elRef).style.offsetPath = `path("M${path}")`;
                         (<any>elRef).style.offsetRotate = "0deg";
-                        elRef.style.animation = `animation-move ${(duration || 0) / 1000}s linear forwards`;
+                        if (path) elRef.style.animation = `animation-move ${(duration || 0) / 1000}s linear forwards`;
 
                         setTimeout(() => {
                             if (animationType !== "show") {
